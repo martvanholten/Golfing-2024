31e3c580e57a95af0cab3b08c9c91ab6450cd533
Mart <martvanholten@hotmail.com>
2025-04-11 11:52:50 +0200

    deploy

--- libs/backend/neo4j/src/lib/neo4j.module.ts
+++ libs/backend/neo4j/src/lib/neo4j.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { Neo4jModule } from 'nest-neo4j';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { UserRepo } from './user/user.repo';
 
 @Module({
-    imports: [Neo4jModule],
-    controllers: [UserController],
-    providers: [UserService],
+    imports: [
+        Neo4jModule
+    ],
+    controllers: [
+        UserController
+    ],
+    providers: [
+        UserService,
+        UserRepo
+    ],
     exports: []
 })
 export class Neo4jBackendModule {}
--- libs/backend/neo4j/src/lib/user/user.service.ts
+++ libs/backend/neo4j/src/lib/user/user.service.ts
@@ -1,16 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Neo4jService } from 'nest-neo4j/dist';
+import { UserRepo } from './user.repo';
 
 @Injectable()
 export class UserService {
     private readonly logger: Logger = new Logger(UserService.name);
 
-    constructor(private readonly neo4jService: Neo4jService) {}
+    constructor(private readonly userRepo: UserRepo) {}
 
     async findAll(): Promise<any> {
         this.logger.log('findAll users');
-        return await this.neo4jService.read(
-            `MATCH (n) RETURN count(n) AS count;`
-        );
+        return await this.userRepo.getCount()
     }
 }
